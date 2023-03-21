6d8e9a794c4828b7716e1143118dd3efd9f2d1c6
Kim Minjae <ffalswo2@naver.com>
2023-03-22 00:26:40 +0900

    [ADD] 사진 받는 input 화면 관련 파일들 추가 (#3)

--- iOS-30/iOS-30/Presentation/Main/PhotoViewController.swift
+++ iOS-30/iOS-30/Presentation/Main/PhotoViewController.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
     }
 
 
