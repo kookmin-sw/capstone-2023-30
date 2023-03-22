14bb5c7d0fd3a0e63f64e2ad2f4d73d9ed204fe5
Kim Minjae <ffalswo2@naver.com>
2023-03-23 02:44:52 +0900

    [Feature] Extension 및 PaddingLabel 추가 (#5)

--- iOS-30/iOS-30/Global/Extension/UIViewController+Extension.swift
+++ iOS-30/iOS-30/Global/Extension/UIViewController+Extension.swift
@@ -5,4 +5,20 @@
 //  Created by 김민재 on 2023/03/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    /**
+     - Description:
+     VC나 View 내에서 해당 함수를 호출하면, 햅틱이 발생하는 메서드입니다.
+     버튼을 누르거나 유저에게 특정 행동이 발생했다는 것을 알려주기 위해 다음과 같은 햅틱을 활용합니다.
+
+     - parameters:
+     - degree: 터치의 세기 정도를 정의. 보통은 medium,light를 제일 많이 활용합니다.
+     */
+    func makeVibrate(degree : UIImpactFeedbackGenerator.FeedbackStyle = .medium)
+    {
+        let generator = UIImpactFeedbackGenerator(style: degree)
+        generator.impactOccurred()
+    }
+}
--- iOS-30/iOS-30/Global/UIComponents/BasePaddingLabel.swift
+++ iOS-30/iOS-30/Global/UIComponents/BasePaddingLabel.swift
@@ -9,12 +9,23 @@ import UIKit
 
 class BasePaddingLabel: UILabel {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private var padding = UIEdgeInsets(top: 16.0, left: 16.0, bottom: 16.0, right: 16.0)
+
+    convenience init(padding: UIEdgeInsets) {
+        self.init()
+        self.padding = padding
     }
-    */
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect)
+    }
+
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+
+        return contentSize
+    }
 
 }
