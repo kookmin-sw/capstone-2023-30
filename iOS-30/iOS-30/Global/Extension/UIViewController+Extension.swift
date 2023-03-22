10861b53f3065e922de957744f6ea2c92bd6de27
Kim Minjae <ffalswo2@naver.com>
2023-03-23 02:42:50 +0900

    [ADD] 카메라 화면 UI 재구성 및 구현 (#5)

---
+++ iOS-30/iOS-30/Global/Extension/UIViewController+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+Extension.swift
+//  iOS-30
+//
+//  Created by 김민재 on 2023/03/23.
+//
+
+import Foundation
---
+++ iOS-30/iOS-30/Global/UIComponents/BasePaddingLabel.swift
@@ -0,0 +1,20 @@
+//
+//  BasePaddingLabel.swift
+//  iOS-30
+//
+//  Created by 김민재 on 2023/03/23.
+//
+
+import UIKit
+
+class BasePaddingLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
